ce74ff398866fd71d72512e8f5b4e7dd2f482906
Your Name <you@example.com>
2021-03-14 14:58:36 +0700

    second commit

---
+++ config.js
@@ -0,0 +1,12 @@
+var settings = {
+  //port service
+  webhookPort: "5200",
+  //authen keys
+  authenticationToken: "mgTN8c2QtlJCjnTBSqd3bHo2R4mRragwGwGASIyDX6k=",
+  //service push data to db
+  serviceIPAdress: "http://192.168.1.104:8086/CCTVEventService",
+  //service push user line to db
+  serviceIPAdressUserLine: "http://192.168.1.104:8086/LineService",
+};
+
+module.exports = settings;
--- index.js
+++ index.js
@@ -4,32 +4,60 @@ let router = express.Router();
 let cors = require("cors");
 let app = express();
 app.use(cors());
+const config = require("./config.js");
+
+const authenticationToken = config.authenticationToken;
+const serviceIPAdress = config.serviceIPAdress;
+const serviceIPAdressUserLine = config.serviceIPAdressUserLine;
 
 // all of our routes will be prefixed with /api
-app.use("/cctv", bodyParser.json(), router); //[use json]
-app.use("/cctv", bodyParser.urlencoded({ extended: false }), router);
+app.use("/hitech/cctv", bodyParser.json(), router); //[use json]
+app.use("/hitech/cctv", bodyParser.urlencoded({ extended: false }), router);
 
 router.route("/webhook").post((req, res) => {
-  console.log("@@@ ----------- testApi -----------");
+  if (!isEmpty(req)) {
+    //check token
+    if (verifyAuthorizationTokenHTTPS(req)) {
+      //
+      //console.log("WEBHOOK_VERIFIED");
+      //console.log("req : ", extractPayloadHTTPS(req));
 
-  if (verifyAuthorizationTokenHTTP(req)) {
-    const data = extractPayloadHTTP(req);
-    console.log(data);
-    res.status(200);
+      if (
+        extractPayloadHTTPS(req).type === "HELLO" ||
+        extractPayloadHTTPS(req).type === "HEARTBEAT"
+      ) {
+        console.log(extractPayloadHTTPS(req).type);
+      } else {
+        //print log event
+        console.log(
+          "event alert & UMD: ",
+          extractPayloadHTTPS(req).notifications[0].event
+        );
+        reply(req);
+        res.status(200);
+      }
+    } else {
+      res.sendStatus(403);
+    }
+  } else {
+    res.sendStatus(403);
   }
 });
 
 router.route("/welcome").get((req, res) => {
-  res.status(200).send("Welcome to CCTV Webhook");
+  res.status(200).send("Welcome to CCTV Webhook v2");
 });
 
-function extractPayloadHTTP(request) {
-  return JSON.parse(request.body);
+function isEmpty(obj) {
+  return !Object.keys(obj).length > 0;
 }
 
+// verify https
+function verifyAuthorizationTokenHTTPS(request) {
+  return authenticationToken === request.body.authenticationToken;
+}
 function verifyAuthorizationTokenHTTP(request) {
   const authenticationTokenBinary = Buffer.from(authenticationToken, "base64");
-
   const authHeader = request.headers.authorization;
   const hmac = crypto.createHmac("sha256", authenticationTokenBinary);
   hmac.update(request.body);
@@ -38,5 +66,34 @@ function verifyAuthorizationTokenHTTP(request) {
   return hashInBase64 === authHeader;
 }
 
+// extract data from payload of body
+function extractPayloadHTTPS(request) {
+  return request.body;
+}
+function extractPayloadHTTP(request) {
+  return JSON.parse(request.body);
+}
+
+//call web api for push data in HiTech(database)
+function reply(req) {
+  console.log("Start psuh event!");
+  const axios = require("axios");
+
+  //const url = "http://192.168.1.199:5300/cctvsvr/event/push";
+  const url = serviceIPAdress + "/cctvsvr/event/push";
+
+  axios
+    .post(url, req.body)
+    .then((res) => {
+      //console.log("Status: ", res.status);
+      //console.log("Body: ", res.data);
+    })
+    .catch((err) => {
+      console.error("Errer : ", err);
+    });
+}
+
 app.use("*", (req, res) => res.status(404).send("404 Not found"));
-app.listen(process.env.PORT || 80, () => console.log("Server is running"));
+app.listen(process.env.PORT || config.webhookPort, () =>
+  console.log("Server is running")
+);
